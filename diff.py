e2f7732e26a944d86f20b3991692e81556fd2d55
jayaprakash-a <akulajayaprakash@gmail.com>
2017-09-04 22:42:18 +0530

    Removed calculation for articles

--- diff.py
+++ diff.py
@@ -34,9 +34,52 @@ def create_dictionary(file_path, my_dictionary):
 			elif word in my_dictionary:
 				my_dictionary[word] += 1
 	
-	print my_dictionary
+	text_document.close()
+	# print my_dictionary
+
+def inner_product(document_one_dictionary, document_two_dictionary):
+	
+	sum = 0
+	
+	for key in document_one_dictionary.keys():
+		if key in document_two_dictionary.keys():
+			sum += document_one_dictionary[key] * document_two_dictionary[key]
+	
+	return sum
+
+def calculate_distance(document_one_dictionary, document_two_dictionary):
+
+	dot_product = inner_product(document_one_dictionary, document_two_dictionary)
+	document_one_norm = inner_product(document_one_dictionary, document_one_dictionary)
+	document_two_norm = inner_product(document_two_dictionary, document_two_dictionary)
+
+	return dot_product / ((document_one_norm * document_two_norm) ** (1/2.0))
+
+def text_formatting(my_dictionary):
+
+	word_file = open('useless_words.txt', 'r')
+	for word in word_file:
+		word = word.rstrip()
+		if word in my_dictionary:
+			my_dictionary[word] = 0
+
+	word_file.close()
+
+	return my_dictionary
 
 if __name__ == "__main__":
-    file1 = "temp.txt"
-    mydict = {}
-    create_dictionary(file1, mydict)
+    document_one = "temp.txt"
+    document_one_dictionary = {}
+    create_dictionary(document_one, document_one_dictionary)
+
+    document_two = "temp2.txt"
+    document_two_dictionary = {}
+    create_dictionary(document_two, document_two_dictionary)
+
+    document_one_dictionary =  text_formatting(document_one_dictionary)
+    document_two_dictionary =  text_formatting(document_two_dictionary)
+
+
+    result = calculate_distance(document_one_dictionary, document_two_dictionary)
+
+    print result
