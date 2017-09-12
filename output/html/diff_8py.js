92d59d982ecafabe78ad488f8d43211e12b206ae
jayaprakash-a <akulajayaprakash@gmail.com>
2017-09-12 18:21:13 +0530

    Documentation for other functions completed.

--- output/html/diff_8py.js
+++ output/html/diff_8py.js
@@ -1,16 +1,27 @@
 var diff_8py =
 [
     [ "calculate_distance", "diff_8py.html#a0b1fc82894743728455b31d3e9ec3671", null ],
-    [ "create_dictionary", "diff_8py.html#a90fd16c78a4c962a1d03912e588c5c7c", null ],
+    [ "create_dictionary", "diff_8py.html#af7396fb95b2a175d067c1c73c75c49f6", null ],
+    [ "ensure_dir", "diff_8py.html#ac7ecd54dde7759dd5569ffb765995278", null ],
     [ "format_text", "diff_8py.html#a17722e85e83540a4710fa571f274e460", null ],
     [ "inner_product", "diff_8py.html#a3a1a3f01a48273670935a0fc45b8975f", null ],
     [ "remove_delimiters", "diff_8py.html#af60a451d448d916b5326b02f0c96147e", null ],
     [ "text_formatting", "diff_8py.html#aa2e19dbdbb5368eafd04b0cfe58c59a9", null ],
     [ "delimiters", "diff_8py.html#a46e2544bf0f797b8d42f4985d4b9bf0a", null ],
-    [ "document_one", "diff_8py.html#aa44a58b96500dd8d5900c22afd849ce1", null ],
-    [ "document_one_dictionary", "diff_8py.html#aa4da046a613de2dafe03d105bca31694", null ],
-    [ "document_two", "diff_8py.html#a64aeca71f753da6e7db7b385ea13e425", null ],
-    [ "document_two_dictionary", "diff_8py.html#a00ac707250a54a82a970c2bb2acf736a", null ],
+    [ "document_dictionary", "diff_8py.html#a7d02a567fcd601900d3960bef0191cea", null ],
+    [ "document_one_dictionary", "diff_8py.html#adb8493e93ac22af9aef08d29989bc40d", null ],
+    [ "document_two_dictionary", "diff_8py.html#ad928e1eb5cb7ebde52f495bf99c78a3b", null ],
+    [ "document_word_count", "diff_8py.html#af16edaaafe4787892cb925870fd0d108", null ],
     [ "end", "diff_8py.html#a2839fdf2e0df35c347f0649098c5ecdc", null ],
-    [ "result", "diff_8py.html#a5df2e8041c6fff30558b49edee35438a", null ]
+    [ "file", "diff_8py.html#a3759e132757b6fdecd117a665d6158ab", null ],
+    [ "file_index_one", "diff_8py.html#ac8ee8a35e457d5004c0f85b0383faf26", null ],
+    [ "folder_path", "diff_8py.html#adccd64c100d50ed0e87d46c33e8636eb", null ],
+    [ "idf_key", "diff_8py.html#a1798972d3d6361a79c16d8470bf3471d", null ],
+    [ "output_folder_path", "diff_8py.html#aed454348ccff9ceb835a6c22de15fa95", null ],
+    [ "result", "diff_8py.html#a5df2e8041c6fff30558b49edee35438a", null ],
+    [ "result_dictionary", "diff_8py.html#a08d29f91abd1bc75aa7d8ecb3aa39c91", null ],
+    [ "result_file", "diff_8py.html#aa1be62f435ce37ca93ba61d4ad88c0c9", null ],
+    [ "result_file_name", "diff_8py.html#a15a863df81a6f3099b088f9f52808389", null ],
+    [ "sorted_result", "diff_8py.html#a3b4c2771da02846b61219801d8016b40", null ],
+    [ "tf_key", "diff_8py.html#ab860e167471d632882c770f12e87927e", null ]
 ];
